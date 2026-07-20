445842c1cd6dfab8c9010a9ee408cb0d3bed443f
paulrponcedeleon-create <paulr.poncedeleon@gmail.com>
2026-07-20 13:18:09 -0600

    feat: curate simulation assets

--- app/config.py
+++ app/config.py
@@ -11,9 +11,8 @@ DEVELOPMENT_SESSION_SECRET = "development-only-change-this-session-secret"
 EXAMPLE_APP_PASSWORD = "replace-with-a-local-password"
 EXAMPLE_SESSION_SECRET = "replace-with-a-random-string-of-at-least-32-characters"
 DEFAULT_SIMULATION_BOOKS = (
-    "btc_mxn,eth_mxn,xrp_mxn,sol_mxn,doge_mxn,ada_mxn,ltc_mxn,link_mxn,"
-    "shib_mxn,pepe_mxn,sui_mxn,hbar_mxn,avax_mxn,dot_mxn,atom_mxn,uni_mxn,"
-    "aave_mxn,mana_mxn,gala_mxn,sand_mxn,bch_mxn,bat_mxn,comp_mxn,mkr_mxn"
+    "btc_mxn,eth_mxn,sol_mxn,atom_mxn,xrp_mxn,paxg_mxn,"
+    "usdc_mxn,usdt_mxn,mxn_cash,algn_mxn,pstg_mxn,tsla_mxn,aapl_mxn"
 )
 
 
